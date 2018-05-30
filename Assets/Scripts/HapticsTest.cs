b0e42391568c5049efb86063fda4980972fea9de
Khamraj Rohit <khamraj@luminousvector.com>
2018-05-30 00:04:37 -0400

    Initial commit

---
+++ Assets/Scripts/HapticsTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HapticsTest : MonoBehaviour
+{
+	SteamVR_ControllerManager cm;
+	SteamVR_Controller controller;
+	public uint device = 1;
+	public uint axis = 0;
+	public float rate = 100;
+	public float nextTick = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time >= nextTick)
+		{
+			OpenVR.System.TriggerHapticPulse(device, axis, char.MaxValue);
+			nextTick = Time.time + rate/1000;
+		}
+	}
+}
---
+++ Assets/Scripts/MagicController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class MagicController : MonoBehaviour
+{
+	public SteamVR_Controller.DeviceRelation deviceRelation = SteamVR_Controller.DeviceRelation.FarthestRight;
+	public int particleRate = 500;
+	private ParticleSystem _ps;
+	private ParticleSystem.EmissionModule _em;
+	private ParticleSystem.Particle[] _particles;
+	private bool _canTrigger = true;
+	public int _controllerID = 1;
+	void Start()
+	{
+		_controllerID = SteamVR_Controller.GetDeviceIndex(deviceRelation, ETrackedDeviceClass.Controller);
+		SteamVR_Controller.Input(_controllerID).TriggerHapticPulse(1000);
+		_ps = GetComponent<ParticleSystem>();
+		_em = _ps.emission;
+		_particles = new ParticleSystem.Particle[_ps.main.maxParticles];
+	}
+
+	void Update()
+	{
+		if (_ps == null)
+			Start();
+
+		var curController = SteamVR_Controller.Input(_controllerID);
+		var trigger = curController.GetAxis(EVRButtonId.k_EButton_SteamVR_Trigger).x;
+		_em.enabled = trigger > .2f;
+		if (curController.GetPressUp(EVRButtonId.k_EButton_SteamVR_Trigger))
+		{
+			_canTrigger = true;
+			var vel = transform.forward * 10;
+			_ps.GetParticles(_particles);
+
+			for (int i = 0; i < _particles.Length; i++)
+			{
+				var p = _particles[i];
+				if (p.remainingLifetime <= 0 || p.velocity != Vector3.zero)
+					continue;
+				p.velocity = vel;
+				p.remainingLifetime = 20;
+				_particles[i] = p;
+			}
+			_ps.SetParticles(_particles, _particles.Length);
+		}
+	}
+}
