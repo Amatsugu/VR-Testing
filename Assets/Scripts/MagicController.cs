3ca4c0bd5e02e5fecc56297d7a1948dbf490032e
Khamraj Rohit <khamraj@luminousvector.com>
2018-07-14 00:31:09 -0400

    Touchpad Division

--- Assets/Scripts/MagicController.cs
+++ Assets/Scripts/MagicController.cs
@@ -13,6 +13,16 @@ public class MagicController : MonoBehaviour
 	private ParticleSystem.Particle[] _particles;
 	private bool _canTrigger = true;
 	public int _controllerID = 1;
+	public TrailRenderer trail;
+
+	public Color fireColor = new Color(1, 0, 0);
+	public Color airColor = new Color(1, 1, .9f);
+	public Color earthColor = new Color(.4f, .2f, 0);
+	public Color voidColor = new Color(1f, 0, .3f);
+	public Color waterColor = new Color(0, .5f, 1f);
+
+	private int _lastSelection;
+	private Color[] _colors;
 	void Start()
 	{
 		_controllerID = SteamVR_Controller.GetDeviceIndex(deviceRelation, ETrackedDeviceClass.Controller);
@@ -20,6 +30,7 @@ public class MagicController : MonoBehaviour
 		_ps = GetComponent<ParticleSystem>();
 		_em = _ps.emission;
 		_particles = new ParticleSystem.Particle[_ps.main.maxParticles];
+		_colors = new[] { voidColor, fireColor, airColor, earthColor, waterColor};
 	}
 
 	void Update()
@@ -53,5 +64,31 @@ public class MagicController : MonoBehaviour
 			}
 			_ps.SetParticles(_particles, _particles.Length);
 		}
+		var touch = curController.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
+		var angle = Angle(touch);
+		var length = touch.magnitude;
+		angle = Mathf.Round(angle / 90);
+		angle = (angle == 4) ? 0 : angle;
+		int selection = (length <= .5f) ? 0 : (int)angle + 1;
+		Debug.Log($"{angle} ,{length},  {selection}");
+		trail.startColor = _colors[selection];
+		trail.endColor = _colors[_lastSelection];
+		if (selection != _lastSelection)
+		{
+			curController.TriggerHapticPulse(1000, EVRButtonId.k_EButton_SteamVR_Touchpad);
+			_lastSelection = selection;
+		}
+	}
+
+	public float Angle(Vector2 vector)
+	{
+		var angle = Mathf.Rad2Deg * Mathf.Atan(vector.y/vector.x);
+		if (float.IsNaN(angle))
+			return 0;
+		if (vector.y < 0 && vector.x >= 0)
+			angle = 360 + angle;
+		if (vector.x < 0)
+			angle = 180 + angle;
+		return angle;
 	}
 }
